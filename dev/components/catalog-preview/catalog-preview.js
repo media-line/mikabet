40a6d524e9d1df0f36025161ef85abe1d7179ea6
gordoney <sergeibolonikov@gmail.com>
2017-07-10 18:12:38 +0300

    10.07

--- dev/components/catalog-preview/catalog-preview.js
+++ dev/components/catalog-preview/catalog-preview.js
@@ -6,6 +6,6 @@ $(window).on('load', function () {
     let blockHeight;
     blockHeight = $($block).outerHeight();
 
-    $($block).css('top', '-' + blockHeight/2 + 'px');
-    $($block).css('margin-bottom', '-' + blockHeight/2 + 'px');
+    $($block).css('top', '-' + blockHeight/100 * 20 + 'px');
+    $($block).css('margin-bottom', '-' + blockHeight/100 * 20 + 'px');
 });
